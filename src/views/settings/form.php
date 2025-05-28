3944ff616f2b9d5591bd76ff9fa70fa041deaa40
Jan-Joost <janjoost@u-lab.nl>
2025-05-28 15:42:07 +0200

    Apply save translation domain

--- src/views/settings/form.php
+++ src/views/settings/form.php
@@ -5,7 +5,7 @@
         <?php wp_nonce_field('teecontrol_settings_verify'); ?>
         <?php
             do_settings_sections('teecontrol');
-    submit_button(__('Save', 'general'));
+    submit_button(__('Save', 'teecontrol'));
     ?>
     </form>
 </div>
