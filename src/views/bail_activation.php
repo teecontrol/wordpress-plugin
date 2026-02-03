77633b0967f94c97aa33cf8a956c3fa300cac28f
jayked <janjoost.heurter@proton.me>
2026-02-03 09:29:26 +0100

    Remove inline script

--- src/views/bail_activation.php
+++ src/views/bail_activation.php
@@ -3,19 +3,6 @@
 
 <head>
     <meta charset="<?php bloginfo('charset'); ?>" />
-    <style>
-        * {
-            text-align: center;
-            margin: 0;
-            padding: 0;
-            font-family: "Lucida Grande", Verdana, Arial, "Bitstream Vera Sans", sans-serif;
-        }
-
-        p {
-            margin-top: 1em;
-            font-size: 18px;
-        }
-    </style>
 </head>
 
 <body>
